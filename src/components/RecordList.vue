e7fe77aebcd2cc0fc6d6caf1077d154dca11282f
ziwenw <ziwenw4900@gmail.com>
2024-07-10 20:03:55 +1200

    default remove to the newest record

--- src/components/RecordList.vue
+++ src/components/RecordList.vue
@@ -1,16 +1,25 @@
 <script setup>
+import {ref, onMounted} from "vue";
 import RecordItem from "./RecordItem.vue"
-const props = defineProps({
+const listDom = ref(null);
+defineProps({
     recordItems: {
         default: () => [],
         type: Array
     }
 })
-console.log(props.recordItems);
+
+
+onMounted(() => {
+    listDom.value?.scrollTo({
+        top: 999999999,
+        behavior: "smooth",
+    })
+});
 </script>
 
 <template>
-    <ul class="list-container">
+    <ul class="list-container" ref="listDom">
         <li v-for="item in recordItems" :key="item.id">
             <RecordItem :timestamp="item.timestamp" :text="item.text" :cid="item.id" />
         </li>
